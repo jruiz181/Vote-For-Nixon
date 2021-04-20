4e0ef0e7368b5bfe8971c5af6512fc76bc05118c
Jeremiah Ruiz <jruiz181@west-mec.org>
2021-04-20 15:27:31 -0700

    about done

--- JS pages/index.js
+++ JS pages/index.js
@@ -19,3 +19,25 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+function george(){
+  var geo = prompt("Please confirm you are voting for McGovern by typing 'Im a loser!'");
+  if(geo == "Im a loser!")
+  modal.style.display = "block";
+  else{
+    prompt("Please confirm you are voting for McGovern by typing 'Im a loser!'")
+  }
+}
+
+span.onclick =  function() {
+  modal.style.display = "none";
+  document.getElementById("geo").src = '../imagesUsed/mcGovern2.jpeg';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementById("geo").src = '../imagesUsed/mcGovern2.jpeg';
+
+  }
+}

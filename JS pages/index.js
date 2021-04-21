d4923434482128506dec1b28b2b614de8ef2f8be
Jeremiah Ruiz <jruiz181@west-mec.org>
2021-04-21 15:28:52 -0700

    done

--- JS pages/index.js
+++ JS pages/index.js
@@ -1,10 +1,46 @@
+window.onload = function(){
+  
+  startUp.style.display = "block";
+  };
+// modal and close
+var startUp = document.getElementById("startUp");
 
-// modal
 var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("rich");
 
 var span = document.getElementsByClassName("close")[0];
+
+var span2 =document.getElementsByClassName("x")[0];
+
+var richImg = document.getElementById("canidate");
+
+var geoImg = document.getElementById("geo");
+
+function mouseover(x){
+  x.style.height = "310px";
+  x.style.transition = ".75s";
+}
+function mouseout(x){
+  x.style.height = "300px";
+  x.style.transition = ".75s";
+
+}
+
+function move(){
+  var btn = document.getElementById("george");
+
+  if(btn.style.hover = true){
+    btn.style.backgroundColor = "#cc0000";
+    btn.style.transition = "2s"
+
+  }
+  
+}
+function moveOut() {
+  var btn = document.getElementById("george");
+  btn.style.backgroundColor = "#0c00b3"
+}
  
 function rich() {
   modal.style.display = "block";
@@ -13,10 +49,25 @@ function rich() {
 span.onclick =  function() {
   modal.style.display = "none";
 }
+span2.onclick = function(){
+  startUp.style.opacity = "0"
+  startUp.style.transition = "1s";
+  x();
+  
+  function x(){
+    startUp.style.display ="none"
+  }
+
+}
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == startUp) {
+    startUp.style.display = "none";
   }
 }
 
@@ -25,9 +76,10 @@ function george(){
   if(geo == "Im a loser!")
   modal.style.display = "block";
   else{
-    prompt("Please confirm you are voting for McGovern by typing 'Im a loser!'")
+    alert("No vote was put in.")
   }
 }
+
 
 span.onclick =  function() {
   modal.style.display = "none";
